e75ed4ae98690a4e1877acae077ff292852fb43f
nshantz <nshantz@asu.edu>
2017-03-12 13:51:09 -0700

    updated routes to use the new schema

--- src/server2.0/routes/content.js
+++ src/server2.0/routes/content.js
@@ -3,12 +3,12 @@ var router = express.Router();
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/content');
 //make sure to include the database name as the 3rd input param or it won't work
-var library_object = mongoose.model('library_object', library_object, 'content');
+var library_object = mongoose.model('ideal_library', ideal_library, 'content');
 
 
 /* Get all subject names */
 router.get('/', function(req, res, next) {
-	library_object.find({},
+	ideal_library.find({},
 		function(err, lo){
 			if(err){
 				res.send(err);
@@ -20,7 +20,7 @@ router.get('/', function(req, res, next) {
 /* Get all items with subject tag */
 router.get('/:subject', function(req, res, next) {
 	console.log(req.params.subject);
-	library_object.find({
+	ideal_library.find({
 		Keywords: req.params.subject
 	}, function(err, lo){
 		if(err) {
@@ -33,7 +33,7 @@ router.get('/:subject', function(req, res, next) {
 
 //returns all file names with the category tag and subject header
 router.get('/:subject/:category', function(req, res, next) {
-	library_object.find({
+	ideal_library.find({
 		Keywords: req.params.subject,
 		Keywords: req.params.category
 	}, function(err, lo){
@@ -46,7 +46,7 @@ router.get('/:subject/:category', function(req, res, next) {
 
 //returns file to open and display
 router.get('/:subject/:category/:file', function(req, res, next) {
-	library_object.find({
+	ideal_library.find({
 		Title: req.params.file
 	}, function(err,lo){
 		if(err){
