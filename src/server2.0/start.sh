bcb047b3d6cad28317c0172933adb65373292b4e
nshantz <nshantz@asu.edu>
2017-04-03 14:56:32 -0700

    Updated shell script to import database file, run mongo, and start node server

--- src/server2.0/start.sh
+++ src/server2.0/start.sh
@@ -3,20 +3,41 @@
 #this starts the mongodb database and the website
 #to import file, run "mongoimport --db SPELL --collection documents <json file> --jsonArray"
 
-case "$(pidof mongod | wc -w)" in
+PCOUNT=`ps -ef | grep -v grep | grep m[o]ngod | wc -l`
 
-0) echo "Starting MongoDB..."
-	sudo mongod &
-	;;
+if [ $PCOUNT -gt 1 ]; then
+	echo "Removed extra instances of Mongo..."
+	ps -ef | grep m[o]ngod | grep -v grep | awk '{print $2}' | xargs kill
 
-1) echo "MongoDB already running..."
-	;;
+else
+	if [ $PCOUNT -eq 0 ]; then
+		echo "Starting MongoDB..."
+		mongod --fork --logpath ./logs/mongodb.log
+	else
+		echo "MongoDB already running..."
+	fi
+fi
 
-*) echo "Removed extra instances of Mongo..."
-	kill $(pidof mongod | awk '{print $1}')
-	;;
 
-esac
+
+#case "$(pgrep -f "mongod" | wc -w)" in
+
+#case "$PCOUNT" in
+
+#0) echo "Starting MongoDB..."
+#	mongod --fork --logpath ./logs/mongodb.log
+#	;;
+
+#1) echo "MongoDB already running..."
+#	;;
+
+#*) echo "Removed extra instances of Mongo..."
+#	kill $(pgrep -f "mongod" | awk '{print $1}')
+#	;;
+
+#esac
+
+mongoimport --db SPELL --collection documents ../prep/content.json --jsonArray
 
 npm install
 npm start
